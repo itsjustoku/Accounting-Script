2bba30fb758c1db1cdfa492eaef4549a1c168cbd
Kaku Baruah <46296585+itsjustoku@users.noreply.github.com>
2021-02-28 19:47:18 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,22 @@
-#!/usr/local/bin
+#!/bin/bash
 apt update
-color 0a
-sleep(1)
+echo -e "\e[1;32;40m"
+sleep 1
 echo "Updating the script, please wait..."
-sleep(1)
-git clone https://github.com/itsjustoku/Accounting-Script.git
-sleep(1.5)
+sleep 1
+cd ..
+echo "Removing tbe old files"
+rm -r Accounting-Script
+sleep 1
 echo "Done !"
-sleep(1.5)
+sleep 1
+echo "Installing new files !"
+git clone https://github.com/itsjustoku/Accounting-Script.git
+sleep 1
+echo "Done !"
+sleep 1
+cd Accounting-Script
 echo "Running the script for you :)"
-sleep(1)
+sleep 1
+chmod +x FA.py
 python3 FA.py

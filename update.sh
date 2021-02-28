98a88336d441ec7a6a929b149db6131d40936c55
Kaku Baruah <46296585+itsjustoku@users.noreply.github.com>
2021-02-28 20:00:10 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,17 +6,19 @@ echo "Updating the script, please wait..."
 sleep 1
 cd ..
 echo "Removing tbe old files"
+sleep 1.5
 rm -r Accounting-Script
-sleep 1
+sleep 1.5
 echo "Done !"
-sleep 1
+sleep 2
 echo "Installing new files !"
 git clone https://github.com/itsjustoku/Accounting-Script.git
-sleep 1
+sleep 2
 echo "Done !"
 sleep 1
 cd Accounting-Script
 echo "Running the script for you :)"
 sleep 1
 chmod +x FA.py
+chmod +x update.sh
 python3 FA.py
